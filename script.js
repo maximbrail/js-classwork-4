10e9412956d3ee5ad20195764edb703eed0ca409
maximbrail <maxabobus953@gmail.com>
2024-07-30 16:47:25 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,77 @@
+// for (let i = 0; i < 10; i++) {
+//     console.log('hello');
+// }
+
+
+
+
+// let i = 0
+// while(i < 5){
+//   console.log(i);
+//   i++
+// }
+
+
+
+
+// let i = 0
+// do {
+//   console.log('Hi');
+//   i++
+// } while (i < 10)
+
+
+
+
+
+// function sayHello(name) {
+//     console.log('Hello World ' + name);
+// }
+
+// sayHello('Max')
+
+
+
+// function sum(a, b) {
+//     console.log(a + b);
+// }
+
+// sum(10, 5)
+
+// sum('Max', 'X')
+
+
+// const gree = function (name) {
+//     console.log('ZXCVBNM<>? ' + name);
+// }
+
+// gree('14')
+
+// const hi = (a, b) => a + b
+// console.log(hi(40, 70));
+
+
+// const hello = (a, b) => a - b
+// console.log(hello(140, 70));
+
+
+// const zdarova = (a, b) => a * b
+// console.log(zdarova(40, 70));
+
+
+// const poka = (a, b) => a / b
+// console.log(poka(160, 400));
+
+// const me = function (a, b, c) {
+//     console.log('Меня зовут ' + a + b + "Мне " + c + " лет");
+// }
+
+// me('Max ', 'Brailov ', 14 )
+
+const me = function (a, b, c) {
+    console.log(`Hello my name is ${a} ${b}, im ${c} years old`);
+}
+
+me('Max', 'Brailov', 14)
+
+
